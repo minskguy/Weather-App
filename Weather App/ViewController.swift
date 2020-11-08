cc59ba05b5bd9c1f6a74ec04ca3b956104583cf5
Mark <macvik17@mail.ru>
2020-11-08 23:13:30 +0300

    "show location" button and "location" label are added

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let button : UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .blue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Detect location", for: .normal)
+        button.addTarget(self, action: #selector(showLocation), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private let location : UILabel = {
+        let location = UILabel()
+        location.translatesAutoresizingMaskIntoConstraints = false
+        location.text = ""
+        location.textColor = .black
+        return location
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(button)
+        view.addSubview(location)
+        addConstraints()
+        
     }
 
+    private func addConstraints() {
+        var constraints = [NSLayoutConstraint]()
+        constraints.append(button.widthAnchor.constraint(equalTo: view.widthAnchor,
+                                                         multiplier: 0.5))
+        constraints.append(button.heightAnchor.constraint(equalTo: view.heightAnchor,
+                                                          multiplier: 0.08))
+        constraints.append(button.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+        constraints.append(button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100))
+        
+        constraints.append(location.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+        constraints.append(location.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50))
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    @objc func showLocation() {
+        if (location.text == "") {
+            location.text = "Minsk, Belarus"
+            button.setTitle("Hide location", for: .normal)
+        }
+        else {
+            location.text = ""
+            button.setTitle("Show location", for: .normal)
+        }
+    }
 
 }
 
